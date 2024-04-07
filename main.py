bfa6d28eeb353de02cfca9a883836876b29d99ab
ShivanshuPrajapati212 <152085574+ShivanshuPrajapati212@users.noreply.github.com>
2024-04-07 09:56:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,16 +5,19 @@ from time import strftime
 
 root = Tk()
 root.title("Clock")
+root.geometry('1950x1200')
+root.minsize(1950,1200)
+root.maxsize(1950,1200)
 
 def time():
-    string = strftime('%H:%M:%S %p')
+    string = strftime('%H:%M:%S')
     label.config(text=string)
     label.after(1000,time)
 
 
 
-label = Label(root,font=('Comicsansm',80),background="black",foreground="cyan",padding=2000)
-label.pack(anchor='center',side='top')
+label = Label(font=('Calibri',250,'bold'),background="black",foreground="cyan",anchor='center',padding=(200,320))
+label.pack(anchor='center',side='top',fill='both')
 time()
 
 mainloop()
